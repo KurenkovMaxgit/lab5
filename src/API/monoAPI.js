1eeb6b87ae0dca9ce9eaa5f38b47623800eb5a4a
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-04-12 23:47:04 +0300

    Update monoAPI.js

--- src/API/monoAPI.js
+++ src/API/monoAPI.js
@@ -4,6 +4,4 @@ const response = await fetch("https://api.monobank.ua/bank/currency")
 currency = await response.json()
 return currency
 }
-// const data = await getMonoCurrency();
-// console.log(data)
 module.exports = {getMonoCurrency};
